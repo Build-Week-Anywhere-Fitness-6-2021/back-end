609928ce0b2107b810815e2c9bfc927a492d0f95
david fletcher <davidfletcheraaron@gmail.com>
2021-12-19 19:08:06 -0500

    first push

---
+++ api/data/migrations/20210124181032_first-migration.js
@@ -0,0 +1,14 @@
+exports.up = async (knex) => {
+  await knex.schema
+    .createTable('users', (users) => {
+      users.increments('user_id')
+      users.string('username', 200).notNullable()
+      users.string('password', 200).notNullable()
+      users.string('email', 320).notNullable()
+      users.timestamps(false, true)
+    })
+}
+
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('users')
+}
