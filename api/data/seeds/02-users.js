4a92d857768d6b09e34f6945aa3e565bf4c5d751
david fletcher <davidfletcheraaron@gmail.com>
2021-12-22 17:26:37 -0500

    set seed data to include hash

--- api/data/seeds/02-users.js
+++ api/data/seeds/02-users.js
@@ -2,8 +2,11 @@
 exports.seed = function(knex) {
       // Inserts seed entries
       return knex('users').insert([
-        {username: 'david', email: 'rowValue1@gmail.com', password:'1234', role_id: 2},
-        {username: 'william', email: 'rowValue2@gmail.com', password:'1234', role_id: 2},
-        {username: 'brettsmith', email: 'brett@smith.com', password:'1234', role_id: 2}
+        {username: 'fletcher_d', email: 'david@fletcher.com', password:'$2a$08$ZPjq/nsa1dNfvFKY54hh.uUGDDHTZcqGnVgq.GVLFSfpaY9K753Im', role_id: 3},
+        {username: 'will', email: 'will@buchanan.com', password:'$2a$08$ZPjq/nsa1dNfvFKY54hh.uUGDDHTZcqGnVgq.GVLFSfpaY9K753Im', role_id: 2},
+        {username: 'brettsmith', email: 'brett@smith.com', password:'$2a$08$ZPjq/nsa1dNfvFKY54hh.uUGDDHTZcqGnVgq.GVLFSfpaY9K753Im', role_id: 2},
+        {username: 'aihe_d', email: 'david@aihe.com', password:'$2a$08$ZPjq/nsa1dNfvFKY54hh.uUGDDHTZcqGnVgq.GVLFSfpaY9K753Im', role_id: 2},
+        {username: 'ted', email: 'ted@velchko.com', password:'$2a$08$ZPjq/nsa1dNfvFKY54hh.uUGDDHTZcqGnVgq.GVLFSfpaY9K753Im', role_id: 2},
+        {username: 'richardo', email: 'richardo@castillo.com', password:'$2a$08$ZPjq/nsa1dNfvFKY54hh.uUGDDHTZcqGnVgq.GVLFSfpaY9K753Im', role_id: 2},
       ]);
 };
