82aeb2e6a8d503a127be735d5ed648406b30b224
david fletcher <davidfletcheraaron@gmail.com>
2021-12-22 17:30:11 -0500

    fixed the users router too -- this will be more bare-bones when i go back to fix

--- api/components/users/users-router.js
+++ api/components/users/users-router.js
@@ -1,20 +1,54 @@
 const router = require('express').Router()
 const User = require('./users-model')
+const bcrypt = require("bcryptjs");
+const { BCRYPT_ROUNDS } = require("../../secrets"); // use this secret!
+
+const {
+   checkUnusedUsername
+} = require('../../auth/auth-middleware')
 
 router.get('/', async (req, res, next)=>{
    res.json( await User.find())
 })
 
-router.post('/', async (req, res, next)=>{
-   const addUser = await User.add(req.body)
-   try{
-      res.status(200).json(addUser)
-      // console.log(req.body)
-      next()
-   } catch(err){
-      next(err)
-   }
-})
+// router.post('/', async (req, res, next)=>{
+//    const addUser = await User.add(req.body)
+//    try{
+//       res.status(200).json(addUser)
+//       // console.log(req.body)
+//       next()
+//    } catch(err){
+//       next(err)
+//    }
+// })
+
+
+router.post("/", checkUnusedUsername, async (req, res, next) => {
+  let user = req.body
+
+  // save on the db
+  const hash = bcrypt.hashSync( user.password, BCRYPT_ROUNDS )
+  user.password = hash
+
+  await User.add(user)
+    .then(saved =>{
+      // res.status(201).json(saved)
+      res.status(201).json({ message: `great to see you, ${saved.username}`})
+    })
+    .catch(next)
+
+  /**
+   [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
+   
+   response:
+   status 201
+   {
+     "user"_id: 3,
+     "username": "anna",
+     "role_name": "angel"
+    }
+    */
+  });
 
 
 module.exports = router
