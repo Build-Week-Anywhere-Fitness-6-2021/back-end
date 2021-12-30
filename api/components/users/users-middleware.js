288e7369b68babe16092ee49a926c3fcd2da3618
david fletcher <davidfletcheraaron@gmail.com>
2021-12-29 23:34:52 -0500

    verification of payload -- for users added

---
+++ api/components/users/users-middleware.js
@@ -0,0 +1,12 @@
+const verifyPayload = (req, res, next) => {
+   const { username, password, email } = req.body
+   if( !username || !password || !email ){
+      return next({ message: 'sorry, you forgot to include necessary information -- either email, password, or username' })
+   } else {
+      next()
+   }
+}
+
+module.exports = {
+   verifyPayload
+}
